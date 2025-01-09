cff347120dea89f4be4c0fd9251763ac029deeb5
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-01-09 23:31:14 +0100

    Add skeleton for the home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import "./App.css"
 
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import { Header } from "./components/atoms/Header"
+import { Header } from "/src/components/atoms/Header"
+import { HomePage } from "/src/pages/Home"
+import { RegisterPage } from "/src/pages/Register"
 
 export const App = () => {
   return (
@@ -10,7 +12,8 @@ export const App = () => {
       <Header />
       <main>
         <Routes>
-          <Route path='/' element={<h2>Add here the home page</h2>} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/register' element={<RegisterPage />} />
           <Route path='*' element={<h2>Add here the not-found page</h2>} />
         </Routes>
       </main>
