40147e75f455232d722eff1e997e4dd94e6eb54f
Maria Zamora <zamora.mariac@gmail.com>
2025-02-08 18:13:52 +0100

    Corrigiendo apellido de Jose

--- src/components/atoms/Footer/Footer.jsx
+++ src/components/atoms/Footer/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   return (
 <footer className="footer footer-center bg-base-300 text-base-content p-4">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by Grupo Rojo (Jose Manilla, Nico Rende, Maria Zamora)</p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Grupo Rojo (Jose Peanilla, Nico Rende, Maria Zamora)</p>
   </aside>
 </footer>
   )
