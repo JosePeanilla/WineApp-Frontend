6fa2a0fef4a917b7cb28bc8bcf0c92fa6c286282
Jose Peanilla <josepeanilla@hotmail.com>
2025-02-20 21:37:56 +0100

    cambio

--- src/components/atoms/Form/FormContainer.jsx
+++ src/components/atoms/Form/FormContainer.jsx
@@ -1,11 +1,11 @@
 export const FormContainer = ({ children, onSubmit }) => (
-    <section className="flex flex-col items-center mt-10">
-      <form
-        onSubmit={onSubmit}
-        className="flex flex-col gap-3 w-[300px]"
-        noValidate
-      >
-        {children}
-      </form>
-    </section>
-  )
+  <section className="flex flex-col items-center">
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col gap-3 w-[300px]"
+      noValidate
+    >
+      {children}
+    </form>
+  </section>
+)
