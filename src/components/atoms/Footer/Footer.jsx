2d02605a7f58bd22e36cd2d14def272561a723df
Jose Peanilla <josepeanilla@hotmail.com>
2025-03-09 14:31:24 +0100

    Agregado logger al footer

--- src/components/atoms/Footer/Footer.jsx
+++ src/components/atoms/Footer/Footer.jsx
@@ -1,10 +1,24 @@
+/************************************************** Internal logger ***************************************************/
+import { Logger } from "/src/utils/Logger.jsx"
+import React, { useEffect } from "react"
+
+const logger = new Logger("Footer")
 
 export const Footer = () => {
+  
+  useEffect(() => {
+    logger.info("Componente Footer montado correctamente.")
+  }, [])
+
   return (
-<footer className="footer footer-center p-4 bg-white">
-  <aside>
-    <p className="text-wineapp-muyfuerte">Copyright © {new Date().getFullYear()} - All right reserved by <span className="text-wineapp-ligero">Grupo Rojo</span> (Jose Peanilla, Nico Rende, Maria Zamora)</p>
-  </aside>
-</footer>
+    <footer className="footer footer-center p-4 bg-white">
+      <aside>
+        <p className="text-wineapp-muyfuerte">
+          Copyright © {new Date().getFullYear()} - All right reserved by 
+          <span className="text-wineapp-ligero"> Grupo Rojo</span> 
+          (Jose Peanilla, Nico Rende, Maria Zamora)
+        </p>
+      </aside>
+    </footer>
   )
 }
